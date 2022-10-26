cc02905e7d3768d6b89daff399c76e778669eb4e
Fahad123k <ahmad.nishu08@gmail.com>
2022-10-26 23:23:00 +0530

    added env success

--- unichat-course-master/src/components/Chats.js
+++ unichat-course-master/src/components/Chats.js
@@ -59,7 +59,7 @@ export default function Chats() {
               axios.post(
                 'https://api.chatengine.io/users',
                 formdata,
-                { headers: { "private-key": process.env.REACT_APP_CHAT_ENGINE_KEY } }
+                { headers: { "private-key":process.env.REACT_APP_CHAT_ENGINE_KEY} }
               )
                 .then(() => setLoading(false))
                 .catch((error) => console.log(error.response))
